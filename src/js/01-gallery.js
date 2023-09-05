8435ae0bfe028b7508c236089a4368d4f40b768c
SergiiBrusov <127529976+SergiiBrusov@users.noreply.github.com>
2023-09-05 11:02:28 +0200

    hw8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,45 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import simpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+const galleryList = document.querySelector('.gallery')
+
+const saveItems = galleryItems.map(item => 
+`<li class = "gallery__item">
+    <a class = "gallery__link" href = "${item.original}">
+    <img
+    class="gallery__image"
+    src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}"
+    />
+    </a>
+</li>`).join('')
+galleryList.insertAdjacentHTML('beforeend', saveItems)
+
+galleryList.addEventListener('click', event => {
+    event.preventDefault();
+    if (event.target.classList.contains('gallery__image')) {
+      const imageUrl = event.target.dataset.source;
+      const imageDesc = event.target.getAttribute('alt');
+      const instance = basicLightbox.create(`<img src="${imageUrl}" alt="${imageDesc}">`,{
+        onShow: () => {
+          document.addEventListener('keydown', closeEscape)
+        },
+        onClose: () => {
+          document.removeEventListener('keydown', closeEscape);
+      }
+    });
+      instance.show();
+      function closeEscape(event){
+        if(event.code === 'Escape'){
+          instance.close()
+        }
+      }
+    }
+})
+
+
 
 console.log(galleryItems);
